8fa763466ba77e7bced612041f0fa34ba760ba48
Steffen Engel <steffen.engel@nea-x.de>
2019-10-14 17:50:04 +0200

    Python3 & refactor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import urllib2
+from urllib import request
 from telegram.ext import CommandHandler, MessageHandler, Filters, InlineQueryHandler
 from telegram.ext import Updater
 import os
@@ -8,31 +8,23 @@ from GameItem import GameItem
 from util import getDate, getUserData
 from telegram_cmd import add_user, latest_game, list_users, remove_user, start
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-userlist = getUserData()
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 def getCurrentGameStats(username):
-    response = urllib2.urlopen('https://euw.op.gg/summoner/userName=' + username)
+    response = request.urlopen(f"https://euw.op.gg/summoner/userName={username}")
     html_doc = response.read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    soup = BeautifulSoup(html_doc, "html.parser")
 
-    tmp = soup.findAll('div', {"class": "GameItemWrap"})
+    tmp = soup.findAll("div", {"class": "GameItemWrap"})
 
     gameStats = []
     for item in tmp:
-        currentStats = item.find('div', {'class': 'GameStats'})
-        gameType = ' '.join(currentStats.find('div', {'class': 'GameType'}).getText().split())
-        timeStamp = getDate(item.find("div", {"class": "TimeStamp"}).find("span")['data-datetime'])
+        currentStats = item.find("div", {"class": "GameStats"})
+        gameType = " ".join(currentStats.find("div", {"class": "GameType"}).getText().split())
+        timeStamp = getDate(item.find("div", {"class": "TimeStamp"}).find("span")["data-datetime"])
         gameStats.append(GameItem(gameType, timeStamp))
 
     return gameStats
-
-if len(userlist) != 0:
-    data = getCurrentGameStats(userlist[0].split('\t')[0])
-    data.sort()
-    if len(data) != 0:
-        print data[0].printFormatedDate()
-
 
 def sayhi(bot, job):
     job.context.message.reply_text("hi")
@@ -43,21 +35,29 @@ def time(bot, update,job_queue):
 def main():
     token = os.getenv("BOT_TOKEN")
     if not token:
-        logging.critical('No token found.')
+        logging.critical("No token found.")
         sys.exit()
+
+    userlist = getUserData()
+
+    if userlist:
+        data = getCurrentGameStats(userlist[0].split("\t")[0])
+        if data:
+            data.sort()
+            print (data[0].printFormatedDate())
     
     updater = Updater(token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.text , time,pass_job_queue=True))
+    dispatcher.add_handler(MessageHandler(Filters.text ,time, pass_job_queue=True))
 
-    dispatcher.add_handler(CommandHandler('add', add_user, pass_args=True))
-    dispatcher.add_handler(CommandHandler('remove', remove_user, pass_args=True))
-    dispatcher.add_handler(CommandHandler('list', list_users))
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('latest', latest_game, pass_args=True))
+    dispatcher.add_handler(CommandHandler("add", add_user, pass_args=True))
+    dispatcher.add_handler(CommandHandler("remove", remove_user, pass_args=True))
+    dispatcher.add_handler(CommandHandler("list", list_users))
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("latest", latest_game, pass_args=True))
 
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -2,14 +2,14 @@ from datetime import datetime
 import pytz
 import os
 
-users_filename = os.getenv('USER_FILE')
-#  'users.txt'
+users_filename = os.getenv("USER_FILE")
+
+UTZ_TZ = pytz.timezone("UTC")
+GMT2_TZ = pytz.timezone("Europe/Berlin")
 
 def getDate(dateAsString):
     utc_date = datetime.utcfromtimestamp(int(dateAsString))
-    old_timezone = pytz.timezone("UTC")
-    new_timezone = pytz.timezone("Europe/Berlin")   
-    tz_date = old_timezone.localize(utc_date).astimezone(new_timezone)
+    tz_date = UTZ_TZ.localize(utc_date).astimezone(GMT2_TZ)
     return tz_date
 
 
@@ -19,48 +19,46 @@ def getUserData():
         users = f.readlines()
     # you may also want to remove whitespace characters like `\n` at the end of each line
     for user in users:
-        userlist.append(user.split('\t')[0].rstrip())
+        userlist.append(user.split("\t")[0].rstrip())
     return userlist
 
-def getLatestGame(username):
+
+def getLatestGame():
     users = getUserData()
 
-
 def appendToUserList(username):
-    with open(users_filename, 'a') as userfile:
-        userfile.write(username)
-        userfile.write('\t')
-        userfile.write(unicode(datetime.now()))
-        userfile.write('\n')
+    with open(users_filename, "a") as userfile:
+        userfile.write(f"{username}\t{str(datetime.now())}\n")
 
 def removeUserFromList(username):
     lines = []
     count = 0
-    with open(users_filename, 'r') as userfile:
+    with open(users_filename, "r") as userfile:
         lines = userfile.readlines()
 
-    with open(users_filename, 'w') as userfile:
+    with open(users_filename, "w") as userfile:
         for line in lines:
-            data = line.split('\t')
-            if data[0]!=username:
+            name, _ = line.split("\t")
+            if name != username:
                 userfile.write(line)
             else:
-                count+=1
+                count += 1
     return count
+
 
 def updateUserList(username, new_date):
     lines = []
     count = 0
 
-    with open(users_filename, 'r') as userfile:
+    with open(users_filename, "r") as userfile:
         lines = userfile.readlines()
 
-    with open(users_filename, 'w') as userfile:
+    with open(users_filename, "w") as userfile:
         for line in lines:
-            data = line.split('\t')
-            if data[0]!=username:
+            name, _ = line.split("\t")
+            if name != username:
                 userfile.write(line)
             else:
-                userfile.write(data[0] + '\t' + new_date + '\n')
-                count+=1
+                userfile.write(f"{name}\t{new_date}\n")
+                count += 1
     return count
